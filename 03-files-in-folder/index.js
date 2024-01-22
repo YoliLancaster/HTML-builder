ed2446c2b0ae4d785acdb6a4e6db814247f7d43f
Yoli <yoli.lancaster@gmail.com>
2024-01-22 14:13:01 +0200

    feat: add task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs').promises;
+const path = require('path');
+const folderPath = path.join(__dirname, 'secret-folder');
+
+async function displayFilesInfo() {
+  try {
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const filePath = path.join(folderPath, file.name);
+        const fileStats = await fs.stat(filePath);
+
+        const fileSize = (fileStats.size / 1024).toFixed(3);
+        const fileName = path.basename(file.name, path.extname(file.name));
+        const fileExt = path.extname(file.name).slice(1);
+        console.log(`${fileName} - ${fileExt} - ${fileSize}kb`);
+      }
+    }
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
+
+displayFilesInfo();
